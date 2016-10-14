5cd3cf2701ca985c009e94f8b07152b257438e42
Richard Dizon <rcd6ab@virginia.edu>
2016-10-13 20:30:55 -0400

    Initial commit

---
+++ parse_xml.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+#from bs4 import BeautifulSoup
+
+#soup = BeautifulSoup(open(sys.argv[1]), 'lxml')
+#model_location = lambda x: x.name == "Model"
+#model = soup(model_location)
+#print(model)
+
+
+f = open('output.txt', 'r')
+while(f.readline()):
+    line = f.readline()
+    if "<packagedElem" in line: 
+        print "Found an element!"
+
